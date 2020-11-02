cf8c31382bfdd26ab6a32117c576f5bc832c0c8d
Andrey150 <69532060+Andrey150@users.noreply.github.com>
2020-11-02 19:46:15 +0300

    Add files via upload

---
+++ pikadu/js/main.js
@@ -0,0 +1,11 @@
+//Создаем переменную, в которую положим кнопку меню
+let menuToggle = document.querySelector('#menu-toggle');
+//Создам переменную, в которую положим меню
+let menu = document.querySelector ('.sidebar');
+//Отслеживаем клик по кнопке меню и запускаем функцию
+menuToggle.addEventListener('click', function(event) {
+    //Отменяем стандартное поведение ссылки
+    event.preventDefault();
+    //вешаем класс на меню, когда кликнули по кнопке меню
+menu.classList.toggle('visible');
+})
